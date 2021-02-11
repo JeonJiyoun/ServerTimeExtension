1f2366841ab43dfbb87efa24c9a50a8ed4587c95
Youn <jiyoun0922@gmail.com>
2021-02-11 19:42:05 +0900

    [feature] 드래그 가능한 익스텐션 기능 추가

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,21 +1,24 @@
 // Content.tsx
-import * as React from 'react';
+import * as React from "react";
+import Draggable from "react-draggable";
 
 interface ContentProps {
   name: string;
 }
 
-const Content: React.FC<ContentProps> = props => {
+const Content: React.FC<ContentProps> = (props) => {
   const { name } = props;
   const style = {
-    textAlign: 'center' as const
-  }
+    textAlign: "center" as const,
+  };
   return (
-    <div style={style}>
-      <h1>{name}의 확장 프로그램</h1>
-      <span>확장 프로그램을 연동해보는 프로젝트 입니다.</span>
-    </div>
+    <Draggable defaultPosition={{ x: 0, y: -100 }}>
+      <div style={style}>
+        <h1>{name}의 확장 프로그램</h1>
+        <span>확장 프로그램을 연동해보는 프로젝트 입니다.</span>
+      </div>
+    </Draggable>
   );
-}
+};
 
 export default Content;
